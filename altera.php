5badc65c8e4c08923f52d7827436f02b2b6ea118
Tecnico em informatica 2016.1 <42115152016.1@senacrjedu.local>
2018-02-06 21:27:12 -0200

    Terceiro teste

---
+++ altera.php
@@ -0,0 +1,63 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <title>BrownieBolos</title>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <!--<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>-->
+  
+  <link rel="stylesheet" href="css/bootstrap.min.css">
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+  <link rel="stylesheet" href="css/estilos.css?aff">
+  <script src="js/jquery.min.js"></script>
+  <script src="js/bootstrap.min.js"></script>
+</head>
+<body>
+<a name="top"></a>
+
+<div class="container">
+		<header class="header">
+		<nav class="navbar navbar-inverse">
+		<div class="container-fluid">
+			<!-- Brand and toggle get grouped for better mobile display -->
+			<!-- BOTÃO RESPONSIVO -->
+			<div class="navbar-header">
+				<button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1" aria-expanded="false">
+				<span class="sr-only">Toggle navigation</span>
+				<span class="icon-bar"></span>
+				<span class="icon-bar"></span>
+				<span class="icon-bar"></span>
+				</button>
+				
+			</div>
+			
+			<div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+				<ul class="nav navbar-nav">
+				<li class="active">
+				
+			<a class="navbar-brand" href="/"><img src="/img/logo.png" style="margin-top:-16px;border-radius:4px"></a>
+		
+		
+			<li class="active"><a href="/">Ínicio</a></li>
+			<li><a href="bolos.html">Bolos</a></li>
+			<li><a href="#">Brownies</a></li>
+			<li><a href="/contatos.php">Contatos</a></li>
+			</ul>
+			<ul class="nav navbar-nav navbar-right">
+			<li><a href="#"><span class="glyphicon glyphicon-user"></span> Entrar</a></li>
+			<li><a href="#"><span class="glyphicon glyphicon-log-in"></span> Login</a></li>
+		</ul>
+		</div>
+	</div>
+	</nav>
+	</header>
+	
+	<div class="searshform">
+		<form>
+			<input placeholder="Procurar"><button><i class="fa fa-search" aria-hidden="true"></i></button>
+		</form>
+	</div>
+	
+	<content>
--- consulta.php
+++ consulta.php
@@ -4,37 +4,7 @@
 <div id="myCarousel" class="carousel slide" data-ride="carousel"> 
 <!-- Indicadores -->
 <ol class="carousel-indicators">
-	<li data-target="#myCarousel" data-slide-to="0" class="active"></li>
-	<li data-target="#myCarousel" data-slide-to="1"></li>
-	<li data-target="#myCarousel" data-slide-to="2"></li>
-</ol>
-
-	<!-- Slides -->
-<div class="carousel-inner">
-    <div class="item active">
-      <img src="img/bolodecenoura.jpg" alt="Bolo de Cenoura" style="width:100%"> 
-      <div class="carousel-caption">
-        <h3>Bolo de Cenoura</h3>
-        <p>Recheio de chocolate!</p>
-      </div>
-    </div>
-
-    <div class="item">
-      <img src="img/Chocolatecreamcake.jpg" alt="Bolo de creme" class="img-responsive" style="width:100%">
-      <div class="carousel-caption">
-        <h3>Bolo de creme</h3>
-        <p>Com chocolate e morango!</p>
-      </div>
-    </div>
-
-    <div class="item">
-      <img src="img/brownies.jpg" alt="Brownie" class="img-responsive" style="width:100%">
-      <div class="carousel-caption">
-        <h3>Brownie</h3>
-        <p>Com calda de Chocolate</p>
-      </div>
-    </div>
-  </div>
+	<li data-target
 
   <!-- Controle do Carrosel -->
   
