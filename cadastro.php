c7cf04a33552a3303a043856bf33cd70fddc3020
Tecnico em informatica 2016.1 <42115152016.1@senacrjedu.local>
2018-02-06 21:24:15 -0200

    Primeiro teste]

---
+++ cadastro.php
@@ -0,0 +1,12 @@
+<?php include "_header.php" ?>
+
+<p>d~gag fbsql_affected_rowssdfg  sdf sdff  df  sda  fbsql_affected_rowsasd  f sad   fbsql_affected_rowssad    f
+<p>d~gag fbsql_affected_rowssdfg  sdf sdff  df  sda  fbsql_affected_rowsasd  f sad   fbsql_affected_rowssad    f
+<p>d~gag fbsql_affected_rowssdfg  sdf sdff  df  sda  fbsql_affected_rowsasd  f sad   fbsql_affected_rowssad    fbsql_affected_rowsasd<p>d~gag fbsql_affected_rowssdfg  sdf sdff  df  sda  fbsql_affected_rowsasd  f sad   fbsql_affected_rowssad    f
+<p>d~gag fbsql_affected_rowssdfg  sdf sdff  df  sda  fbsql_affected_rowsasd  f sad   fbsql_affected_rowssad    f
+<p>d~gag fbsql_affected_rowssdfg  sdf sdff  df  sda  fbsql_affected_rowsasd  f sad   fbsql_affected_rowssad    f
+<p>d~gag fbsql_affected_rowssdfg  sdf sdff  df  sda  fbsql_affected_rowsasd  f sad   fbsql_affected_rowssad    f
+<p>d~gag fbsql_affected_rowssdfg  sdf sdff  df  sda  fbsql_affected_rowsasd  f sad   fbsql_affected_rowssad    f
+<p>d~gag fbsql_affected_rowssdfg  sdf sdff  df  sda  fbsql_affected_rowsasd  f sad   fbsql_affected_rowssad    f
+
+<?php include "_footer.php" ?>
---
+++ consulta.php
@@ -0,0 +1,89 @@
+<?php include "_header.php" ?>
+
+
+<div id="myCarousel" class="carousel slide" data-ride="carousel"> 
+<!-- Indicadores -->
+<ol class="carousel-indicators">
+	<li data-target="#myCarousel" data-slide-to="0" class="active"></li>
+	<li data-target="#myCarousel" data-slide-to="1"></li>
+	<li data-target="#myCarousel" data-slide-to="2"></li>
+</ol>
+
+	<!-- Slides -->
+<div class="carousel-inner">
+    <div class="item active">
+      <img src="img/bolodecenoura.jpg" alt="Bolo de Cenoura" style="width:100%"> 
+      <div class="carousel-caption">
+        <h3>Bolo de Cenoura</h3>
+        <p>Recheio de chocolate!</p>
+      </div>
+    </div>
+
+    <div class="item">
+      <img src="img/Chocolatecreamcake.jpg" alt="Bolo de creme" class="img-responsive" style="width:100%">
+      <div class="carousel-caption">
+        <h3>Bolo de creme</h3>
+        <p>Com chocolate e morango!</p>
+      </div>
+    </div>
+
+    <div class="item">
+      <img src="img/brownies.jpg" alt="Brownie" class="img-responsive" style="width:100%">
+      <div class="carousel-caption">
+        <h3>Brownie</h3>
+        <p>Com calda de Chocolate</p>
+      </div>
+    </div>
+  </div>
+
+  <!-- Controle do Carrosel -->
+  
+  <a class="left carousel-control" href="#myCarousel" data-slide="prev">
+    <span class="glyphicon glyphicon-chevron-left"></span>
+    <span class="sr-only">Anterior</span>
+  </a>
+  <a class="right carousel-control" href="#myCarousel" data-slide="next">
+    <span class="glyphicon glyphicon-chevron-right"></span>
+    <span class="sr-only">Proximo</span>
+  </a>
+</div>
+  
+<div>
+  <div class="row">
+    <div class="col-sm-4">
+      <h3> Bolo de Cereja</h3> 
+		<a href="#"><img src="img/bolo2.jpg" alt="Bolo de Cereja" class="img-responsive" ></a>
+      
+    </div>
+    <div class="col-sm-4">
+      <h3>Bolo de Casamento</h3>
+	   <a href="#"><img src="img/bolocoracao.jpg" alt="Bolo de Casamento" class="img-responsive"></a>
+      
+    </div>
+    <div class="col-sm-4">
+      <h3>Brownie Crocante</h3>
+		<a href="#"><img src="img/large1.jpg" alt="Brownie" class="img-responsive"></a>
+		
+	</div>
+	</div>
+</div>	
+	<div class="row">
+    <div class="col-sm-4">
+      <h3>Bolo de Morango Divino</h3>
+		<a href="#"><img src="img/bolodemorango.jpg" alt="Bolo de Morango Divino" class="img-responsive"></a>
+      
+    </div>
+    <div class="col-sm-4">
+      <h3>Bolo de Morango Divino</h3>
+		<a href="#"><img src="img/bolodemorango.jpg" alt="Bolo de Morango Divino" class="img-responsive"></a>
+      
+    </div>
+    <div class="col-sm-4">
+      <h3>Brownie de Pizza</h3>
+		<a href="#"><img src="img/bolopizza.jpg" alt="Brownie de Pizza" class="img-responsive"></a>
+      
+    </div>
+  </div>
+
+
+<?php include "_footer.php" ?>
