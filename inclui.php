3e97c98ac6c6f2b0ca2a71e467a92942ca60cfcb
Tecnico em informatica 2016.1 <42115152016.1@senacrjedu.local>
2018-02-06 21:24:52 -0200

    Segundo teste

---
+++ inclui.php
@@ -0,0 +1,22 @@
+  </content>
+&nbsp;
+	<div class="footer navbar navbar-inverse row" style="margin:0">
+		<div class="col-sm-2"><a href="/"><i class="fa fa-fw fa-home" aria-hidden="true"></i></a></div>
+		<div class="col-sm-8 text-center">
+			<i class="glyphicon glyphicon-copyright-mark"></i> Copyright 2018 Welington e Gabriel
+			<div class="social">
+				<a href="http://facebook.com"><i class="fa fa-fw fa-facebook-official" aria-hidden="true"></i>&nbsp;
+				<a href="http://instagram.com"><i class="fa fa-fw fa-instagram" aria-hidden="true"></i>&nbsp;
+				<a href="http://twitter.com"><i class="fa fa-fw fa-twitter" aria-hidden="true"></i></a>&nbsp;
+				<a href="contatos.php"><i class="fa fa-fw fa-envelope-o" aria-hidden="true"></i></a>
+			</div>
+		</div>
+		<div class="col-sm-2 text-right"><a href="#top"><i class="fa fa-fw fa-arrow-circle-up" aria-hidden="true"></i></a></div>
+	</div>
+
+  
+  </div>
+	
+
+</body>
+</html>
